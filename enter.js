36531c1d509b86c6b9297c0abfc153c5d8c3a2c6
Johnathan Coe <jcoe148@aucklanduni.ac.nz>
2021-04-10 15:32:15 +1200

    The co-ordinates given by NetGuard are in the form of an image with no alt text. We will use Tesseract to perform OCR on these.

--- enter.js
+++ enter.js
@@ -1,1 +1,13 @@
-console.log("Hello, AutoNetGuard!")
+function enter() {
+    // Find the relevant prompt images
+    // Run through OCR
+    
+}
+
+// Include an OCR library (Tesseract)
+var imported = document.createElement('script');
+imported.src = 'https://cdn.rawgit.com/naptha/tesseract.js/0.2.0/dist/tesseract.js';
+document.head.appendChild(imported);
+
+// On page load, run the script
+object.addEventListener("load", enter);
