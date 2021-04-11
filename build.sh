a5d3fc18b713f928d5c8486562d7b4f70eb43f92
Johnathan Coe <jcoe148@aucklanduni.ac.nz>
2021-04-11 13:45:59 +1200

    Move content script and associated files into content_script/

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-browserify enter.js -o bundle.js
+# Ensure Tesseract is installed
+npm install --prefix content_script/
+browserify content_script/enter.js -o content_script/bundle.js
 web-ext build --overwrite-dest
